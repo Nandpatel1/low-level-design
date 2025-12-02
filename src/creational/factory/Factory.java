925f49cc318670dd5f02ef55abb21b95b358fe21
Nand <nandpatel01218@gmail.com>
2025-12-02 23:17:36 +0530

    Factory Pattern

--- src/creational/factory/Factory.java
+++ src/creational/factory/Factory.java
@@ -1,4 +1,81 @@
 package creational.factory;
 
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+}
+
+// Factory Class
+class ShapeFactory {
+    /* Method that takes the type of shape as input
+    and returns the cirresponding object */
+    public Shape getShape(String shapeType) {
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
+
+
+
+// -------------------------------- Real life example --------------------------------
+interface Logistics {
+    void send();
+}
+
+class Road implements Logistics {
+    @Override
+    public void send() {
+        System.out.println("Sending by road logic");
+    }
+}
+
+class Air implements Logistics {
+    @Override
+    public void send() {
+        System.out.println("Sending by air logic");
+    }
+}
+
+class LogisticsFactory {
+    public static Logistics getLogistics(String mode) {
+        if (mode.equalsIgnoreCase("Air")) {
+            return new Air();
+        } else if (mode.equalsIgnoreCase("Road")) {
+            return new Road();
+        }
+        throw new IllegalArgumentException("Unknown logistics mode: " + mode);
+    }
+}
+
+// Class implementing the Logistics Services
+class LogisticsService {
+    public void send(String mode) {
+        /* Using the Logistics Factory to get the
+        desired object based on the mode */
+        Logistics logistics = LogisticsFactory.getLogistics(mode);
+        logistics.send();
+    }
+}
+
 public class Factory {
+    public static void main(String[] args) {
+
+    }
 }
